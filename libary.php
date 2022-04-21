68b09f4f80c78fa9c51131c2bb20d51f4b5455d3
Jared <103985402+Jaredneoh@users.noreply.github.com>
2022-04-21 17:37:33 +0800

    Add files via upload

---
+++ libary.php
@@ -0,0 +1,33 @@
+<?php
+// database connection code
+if(isset($_POST['txtBook']))
+{
+// $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
+$con = mysqli_connect('localhost', 'root', '','library');
+
+// get the post records
+
+$txtBook = $_POST['txtBook'];
+$txtAuthor = $_POST['txtAuthor'];
+$txtBp = $_POST['txtBp'];
+$txtPub = $_POST['txtPub'];
+$txtIsbn = $_POST['txtIsbn'];
+$txtCpr = $_POST['txtCpr'];
+$txtDate = $_POST['txtDate'];
+
+// database insert SQL code
+$sql = "INSERT INTO `tbl_contact` (`book_id`, ``book_title`, `category_id`, `author`, `book_copies`,book_pub`, `publisher_name`, `isbn`, `copyright`, `date_added`, `status`) VALUES ('0', '$txtBook', '0', '$txtAuthor', '0', '$txtBp', '$txtPub', '$txtMessage', '$txtIsbn', '$txtCpr', '$txtDate', '0')";
+
+// insert in database 
+$rs = mysqli_query($con, $sql);
+if($rs)
+{
+	echo "Book Details Inserted";
+}
+}
+else
+{
+	echo "Are you a genuine visitor?";
+	
+}
+?>
