ec5953bb19c88cd869137b6dbbe08dfec16bdd31
iikoiiko <103985413+iikoiiko@users.noreply.github.com>
2022-04-23 04:25:42 +0800

    Update inAddBooks.php

--- inAddBooks.php
+++ inAddBooks.php
@@ -18,21 +18,24 @@ $conn =new mysqli($servername, $username,$password,$dbname);
 // get the post records
 
 $txtBook = $_POST['txtBook'];
+$txtcat = $_POST['txtcat'];
 $txtAuthor = $_POST['txtAuthor'];
 $txtBp = $_POST['txtBp'];
 $txtPub = $_POST['txtPub'];
 $txtIsbn = $_POST['txtIsbn'];
 $txtCpr = $_POST['txtCpr'];
 $txtDate = $_POST['txtDate'];
+$txtStat = $_POST['status'];
 
 // database insert SQL code
-$sql = "INSERT INTO `book` (`book_id`, ``book_title`, `category_id`, `author`, `book_copies`,book_pub`, `publisher_name`, `isbn`, `copyright`, `date_added`, `status`) VALUES ('0', '$txtBook', '0', '$txtAuthor', '0', '$txtBp', '$txtPub', '$txtMessage', '$txtIsbn', '$txtCpr', '$txtDate', '0')";
+$sql = "INSERT INTO book (`book_id`, `book_title`, `category_id`, `author`, `book_copies`,`book_pub`, `publisher_name`, `isbn`, `copyright`, `date_added`, `status`) VALUES ('0', '$txtBook', '$txtcat' , '$txtAuthor', '0', '$txtBp', '$txtPub', '$txtIsbn', '$txtCpr', '$txtDate', '$txtStat')";
 
 // insert in database 
 
 if(mysqli_query($conn, $sql))
 {
-	echo "Book Details Inserted";
+	header("refresh:3;Library.php");
+	echo "Book Details Inserted! Redirecting to Home Page in 3 seconds...";
 }
 else
 {
