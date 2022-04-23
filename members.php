8257a4c307141fc40c09347a37fbfb366f99d566
iikoiiko <103985413+iikoiiko@users.noreply.github.com>
2022-04-23 18:24:32 +0800

    Update members.php

--- members.php
+++ members.php
@@ -26,9 +26,9 @@ body {
         <nav>
             <h1 class="logo">The Libary System</h1>
             <ul>
-              <li><a href="addBooks.html">AddBooks</a></li>
+              <li><a href="addBooks.php">AddBooks</a></li>
               <li><a href="SearchBooks.html">SearchBooks</a></li>
-              <li><a href="Library.html">Home</a></li>
+              <li><a href="Library.php">Home</a></li>
               <li><a href="SearchMembers.html">SearchMembers</a></li>
               <li><a href="BorrowingDetails.html">Borrowing Details</a></li>
               <li><a href="DeleteBooks.html">DeleteBooks</a></li>
@@ -47,7 +47,7 @@ body {
       </div>
 <fieldset>
   
-  <form name="frmContact" class="needs-validation " method="post" action="libary.php">
+  <form name="frmContact" class="needs-validation " method="post" action="inMembers.php">
     <p>
       <label for="First Name">First Name</label>
       <input type="text" class="form-control" name="txtfname" id="txtfname" placeholder="First Name" value="" required>
@@ -67,6 +67,27 @@ body {
       <label for="Address">Address</label>
       <input type="text" class="form-control"  name="txtAddress" id="txtAddress"  placeholder="Address" value="" required>
     </p>
+    <p>
+      <label for="Type">Member Type:</label>
+		<select name="Type" id="Type">
+			<option value="Teacher">Teacher</option>
+			<option value="Employee">Employee</option>
+      <option value="Non-Teaching">Non-Teaching</option>
+      <option value="Student">Student</option>
+      <option value="Construction">Construction</option>
+		</select>
+    </p>
+    <p>
+      <label for="txtYear">Year level:</label>
+		<select name="txtYear" id="txtYear">
+			<option value="First Year">First Year</option>
+			<option value="Second Year">Second Year</option>
+      <option value="Third Year">Third Year</option>
+      <option value="Fourth Year">Fourth Year</option>
+      <option value="Faculty">Faculty</option>
+		</select>
+    </p>
+    <p>
 	<p>
       <label for="Contact">Contact</label>
       <input type="number" class="form-control"  name="txtContact" id="txtContact"  placeholder="Contact Number" value="" required>
@@ -82,3 +103,4 @@ body {
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 </body>
 </html>
+
